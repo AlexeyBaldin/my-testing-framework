fbacbbeca9553cc2ac35120d06da85cf149603f6
Alexey <kret906alex@gmail.com>
2022-06-23 10:57:54 +0300

    Pom update 1

--- src/main/java/org/mytestingframework/Main.java
+++ src/main/java/org/mytestingframework/Main.java
@@ -1,7 +1,11 @@
 package org.mytestingframework;
 
 import org.mytestingframework.MyTestingFramework;
+import org.mytestingframework.annotations.MyTest;
+import org.mytestingframework.annotations.MyTesterTarget;
+import static org.mytestingframework.constant.Color.*;
 
+@MyTesterTarget
 public class Main {
     public static void main(String[] args) {
         try {
@@ -9,5 +13,11 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @MyTest
+    public static boolean testSuccess() {
+        System.out.println(ANSI_BLUE + "Test success" + ANSI_RESET);
+        return true;
     }
 }
