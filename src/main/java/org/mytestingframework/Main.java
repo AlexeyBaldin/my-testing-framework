4e0ee3dfd1447c1eff601d7b6ac90eabfb06c6e2
Alexey <kret906alex@gmail.com>
2022-06-30 11:35:59 +0300

    Test for github actions

--- src/main/java/org/mytestingframework/Main.java
+++ src/main/java/org/mytestingframework/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             MyTestingFramework.run("");
+            System.out.println("test");
         } catch (Exception e) {
             e.printStackTrace();
         }
