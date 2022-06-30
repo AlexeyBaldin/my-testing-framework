7e672aec199afe2e9f5b3d83e31e16a08380975c
Alexey <kret906alex@gmail.com>
2022-06-30 11:45:04 +0300

    Test for github actions dev 2(1.0.7)

--- src/main/java/org/mytestingframework/Main.java
+++ src/main/java/org/mytestingframework/Main.java
@@ -15,6 +15,8 @@ public class Main {
             System.out.println("test");
             System.out.println("test");
             System.out.println("test");
+            System.out.println("test");
+            System.out.println("test");
         } catch (Exception e) {
             e.printStackTrace();
         }
